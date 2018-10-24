7cb88337040f0615f89c1edd2409a73b46020c07
Dominik <dominikfijalek@gmail.com>
2018-10-24 23:26:38 +0200

    Added main site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CompanyComponent } from './company/company.component';
+import { OfferComponent } from './offer/offer.component';
+import { ContactComponent } from './contact/contact.component';
+import {Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+    {
+
+    },
+    {
+
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CompanyComponent,
+    OfferComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
